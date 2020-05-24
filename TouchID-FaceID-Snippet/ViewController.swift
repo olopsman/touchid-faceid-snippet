94a1ee73ff8e604414116d2f923cb20ec7ed01eb
Paulo Orquillo <paulo.orquillo@davanti.co.nz>
2020-05-24 14:59:35 +1200

    Initial Commit

---
+++ TouchID-FaceID-Snippet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TouchID-FaceID-Snippet
+//
+//  Created by Paulo Orquillo on 24/05/20.
+//  Copyright © 2020 Quonsepto Limited. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
